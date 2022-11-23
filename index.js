2f667a1fd7270de4ae6b76d6b96e08dcd10a113c
James Lorence <james.seril.12@gmail.com>
2022-11-23 21:31:54 +0800

    Changes to be committed: 	modified:   index.js
    Changes not staged for commit:
    	modified:   node_modules/.package-lock.json
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   views/index.ejs
    
    Untracked files:
    	node_modules/.bin/fxparser
    	node_modules/.bin/fxparser.cmd
    	node_modules/.bin/fxparser.ps1
    	node_modules/.bin/uuid
    	node_modules/.bin/uuid.cmd
    	node_modules/.bin/uuid.ps1
    	node_modules/@aws-crypto/
    	node_modules/@aws-sdk/
    	node_modules/@types/
    	node_modules/base64-js/
    	node_modules/bowser/
    	node_modules/bson/
    	node_modules/buffer/
    	node_modules/denque/
    	node_modules/fast-xml-parser/
    	node_modules/ieee754/
    	node_modules/ip/
    	node_modules/kareem/
    	node_modules/memory-pager/
    	node_modules/mongodb-connection-string-url/
    	node_modules/mongodb/
    	node_modules/mongoose/
    	node_modules/mpath/
    	node_modules/mquery/
    	node_modules/punycode/
    	node_modules/saslprep/
    	node_modules/sift/
    	node_modules/smart-buffer/
    	node_modules/socks/
    	node_modules/sparse-bitfield/
    	node_modules/strnum/
    	node_modules/tr46/
    	node_modules/tslib/
    	node_modules/uuid/
    	node_modules/webidl-conversions/
    	node_modules/whatwg-url/
    	views/register.ejs
    	views/signin.ejs

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const express = require('express')
+// const mongoose = require('mongoose')
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser'); 
 
 const app = express()
 
-app.set('view engine',"ejs")
+app.set('view engine','ejs')
 
 app.get('/',(req,res) => {
     res.render("index")
 })
 
+// signin page
+app.get('/signin', (req,res) => {
+    res.render('views/signin');
+})  
+
+// access public folder
 app.use('/public', express.static('public'));
 
+// Start  the server
 app.listen(3000,() => {
     console.log("App is listening on port 3000..")
 
